8f8d817e8c8815ea5868afbc7829161bb1999a58
smalldog124 <panumars124@gmail.com>
2020-03-31 23:53:33 +0700

    [Added]: เพิ่มเคสของการส่ง array ว่างเข้าไป

--- same-array/same.go
+++ same-array/same.go
@@ -1,6 +1,9 @@
 package same
 
 func Comp(array1 []int, array2 []int) bool {
+	if len(array1) == 0 || len(array2) == 0 {
+		return false
+	}
 	lenth := len(array1) - 1
 	for i := 0; i < lenth; i++ {
 		n := array1[i]
--- same-array/same_test.go
+++ same-array/same_test.go
@@ -26,3 +26,13 @@ func Test_Comp_Input_Array_A_And_B_Should_Be_False(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func Test_Comp_Input_Array_A_nil_And_B_Should_Be_False(t *testing.T) {
+	expected := false
+	var a []int
+	b := []int{11 * 21, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19}
+
+	actual := same.Comp(a, b)
+
+	assert.Equal(t, expected, actual)
+}
