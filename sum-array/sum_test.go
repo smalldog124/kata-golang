1eab2ad88344f455150ee958160e81beb80d4f3e
smalldog124 <panumars124@gmail.com>
2020-04-04 02:33:37 +0700

    [Added]: เพิ่มเทสเคสไปที่ sum-array

--- sum-array/sum_test.go
+++ sum-array/sum_test.go
@@ -15,3 +15,12 @@ func Test_Call_Input_Array_0_1_3_6_10_Should_Be_20_20_19_16_10_0(t *testing.T) {
 
 	assert.Equal(t, actual, expected)
 }
+
+func Test_Call_Input_Array_1_2_3_4_5_6_Should_Be_21_20_18_15_11_6_0(t *testing.T) {
+	expected := []int{21, 20, 18, 15, 11, 6, 0}
+	input := []int{1, 2, 3, 4, 5, 6}
+
+	actual := sum.Call(input)
+
+	assert.Equal(t, actual, expected)
+}
