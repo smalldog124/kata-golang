9a49bd107ef895b469a4dc78951e87b9478c6f8a
smalldog124 <panumars124@gmail.com>
2020-04-05 12:03:05 +0700

    [Edited]: ปรับ code ให้ big o = O(n) ที่ไฟล์ sum-array

--- sum-array/sum.go
+++ sum-array/sum.go
@@ -1,14 +1,13 @@
 package sum
 
 func Call(array []int) []int {
-	length := len(array)
-	var partSum []int
-	for i := 0; i < length; i++ {
-		var sum int
-		for j := i; j < length; j++ {
-			sum += array[j]
-		}
-		partSum = append(partSum, sum)
+	length := len(array) - 1
+	partSum := array
+	var sum int
+	for i := length; i >= 0; i-- {
+		sum += array[i]
+		partSum[i] = sum
 	}
+
 	return append(partSum, 0)
 }
